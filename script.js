fbedfa511f8b9964d538f3990b22d22c77b6c02e
Terren Handy <213094058+thandy1@users.noreply.github.com>
2025-08-05 18:07:08 -0500

    Add js logic for human choice and initialized score variables

--- script.js
+++ script.js
@@ -8,5 +8,17 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
-// Write the logic to get the human choice choice
 // console.log(getComputerChoice());
+
+// Write the logic to get the human choice choice
+function getHumanChoice() {
+    prompt("Rock, Paper, or Scissors?");
+}
+// Dont worry about handling invalid input, assume the input is correct
+// console.log(prompt("Rock, Paper, or Scissors?")); returns correctly
+
+// Declare the players score variables
+let humanScore = 0;
+let computerScore = 0;
+
+// Write the logic to play single round
