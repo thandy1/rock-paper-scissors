2dea8d0b2074211b3659ad1619b5e23de17d73c9
Terren Handy <213094058+thandy1@users.noreply.github.com>
2025-08-05 17:50:00 -0500

    Add the logic for the computer choice
    This function will determine what string will get randomly returned depending on the computers choice.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Write the logic to get the computer choice
+function getComputerChoice() {
+    if (Math.random() > 0) {
+        return "Rock";
+    } else if (Math.random() < 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+// Write the logic to get the human choice choice
+// console.log(getComputerChoice());
