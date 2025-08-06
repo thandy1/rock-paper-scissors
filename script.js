76df1f88af5d8a845fbc6519a73483a6ce7632d6
Terren Handy <213094058+thandy1@users.noreply.github.com>
2025-08-05 19:34:00 -0500

    Snapshot the current program to reuse if needed

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Write the logic to get the computer choice
 function getComputerChoice() {
-    if (Math.random() > 0) {
+    if (Math.random(Math.floor) >= 60) {
         return "Rock";
-    } else if (Math.random() < 1) {
+    } else if (Math.floor(Math.random() * 100) <= 30) {
         return "Paper";
-    } else {
+    } else if (Math.floor(Math.random() * 100) >= 31 ) {
         return "Scissors";
     }
-}
+        
+    }
 // console.log(getComputerChoice());
 
 // Write the logic to get the human choice choice
@@ -20,5 +21,44 @@ function getHumanChoice() {
 // Declare the players score variables
 let humanScore = 0;
 let computerScore = 0;
-
-// Write the logic to play single round
+// Write the logic to play the entire game
+function playGame() {
+    // Write the logic to play single round
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == /rock/i && computerChoice == /rock/i) {
+            console.log("Tie");
+        } else if (humanChoice == /paper/i && computerChoice == /paper/i) {
+            console.log("Tie");
+        } else if (humanChoice == /scissors/i && computerChoice == /scissors/i) {
+            console.log("Tie");
+        } else if (humanChoice == /rock/i && computerChoice == /paper/i) {
+            computerScore++;
+            console.log("Paper beats Rock, Computer Wins!");
+        } else if (humanChoice == /rock/i && computerChoice == /scissors/i) {
+            humanScore++;
+            console.log("Rock beats scissors, Human Wins!");
+        } else if (humanChoice == /paper/i && computerChoice == /rock/i) {
+            humanScore++;
+            console.log("Paper beats rock, Human Wins!");
+        } else if (humanChoice == /paper/i && computerChoice == /scissors/i) {
+            computerScore++;
+            console.log("Scissors beats paper, Computer Wins!");
+        } else if (humanChoice == /scissors/i && computerChoice == /rock/i) {
+            computerScore++;
+            console.log("Rock beats scissors, Computer Wins!");
+        } else if (humanChoice == /scissors/i && computerChoice == /paper/i) {
+            humanScore++;
+            console.log("Scissors beats paper, Human Wins!");
+        } else {
+            console.log("Game has stopped");
+        }
+    }
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection); 
+    playRound(humanSelection, computerSelection); 
+    playRound(humanSelection, computerSelection); 
+    playRound(humanSelection, computerSelection); 
+    playRound(humanSelection, computerSelection); 
+    playRound(humanSelection, computerSelection); 
+}
