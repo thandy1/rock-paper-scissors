f1af78588aeedd33425a55c582c3b8fed5f6fd65
Terren Handy <213094058+thandy1@users.noreply.github.com>
2026-04-28 20:51:48 -0500

    Add core game logic and screen navigation
    - Screen navigation working
    - Computer choice logic
    - Win conditions logic
    - Choice button event listeners wired up
    - playGame() flow mapped out

--- script.js
+++ script.js
@@ -1,14 +1,66 @@
+// DOM references
 const startButton = document.getElementById("start-button");
 const mainMenu = document.getElementById("main-menu");
 const gameScreen = document.getElementById("game-screen");
 const homeButton = document.getElementById("home-button");
+const choiceButtons = document.querySelectorAll(".choice-button");
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("computer-score");
 
-startButton.addEventListener(onclick, function() {
+
+choiceButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        let playerChoice = button.dataset.choice;
+        playGame(playerChoice);
+    });
+});
+
+
+// Each key beats its value
+const winConditions = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper"
+}
+
+
+startButton.addEventListener("click", function() {
     mainMenu.style.display = "none";
     gameScreen.style.display = "block";
 });
 
-homeButton.addEventListener(onclick, function() {
+
+homeButton.addEventListener("click", function() {
     mainMenu.style.display = "block";
     gameScreen.style.display = "none";
 });
+
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return "rock";
+    } else if  (computerChoice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+
+function playGame(playerChoice) {
+    let computerChoice = getComputerChoice();
+    compareChoice(playerChoice, computerChoice);
+}
+
+
+function compareChoice(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        // tie
+    } else if (winConditions[playerChoice] === computerChoice) {
+        // player wins
+    } else {
+        // computer wins
+    }
+}
