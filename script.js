4c9d6e0ecbadbd1f73c17373b7e2c4b176634071
Terren Handy <213094058+thandy1@users.noreply.github.com>
2026-05-04 21:48:49 -0500

    Add hand display and animations
    - Hand display area to show the current player and computer hand choices
    - Animation mimicking a shake before showing the hand choices
    - Implemented time delay on animations to give the UI time to update after animations finish
    - Added a guard to prevent users from interacting with buttons during animations
    - Added playAnimation function to play the hand animation on event clicks
    - Changed some UI elements for visual hierarchy

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict"    // Directive tells the JS engine to user stricter rules to avoid JS very permissive behavior.
+
 // DOM references
 const quitButton = document.getElementById("quit-button");
 const choiceButtons = document.querySelectorAll(".choice-button");
@@ -8,12 +9,16 @@ const computerScore = document.getElementById("computer-score");
 const roundIndicator = document.getElementById("round-indicator");
 const playAgainButton = document.getElementById("play-again-button");
 const choices = document.getElementById("choices");
+const handDisplay = document.getElementById("hand-display");
+const playerHand = document.getElementById("player-hand");
+const computerHand = document.getElementById("computer-hand");
 
 
 // Globals to track game state.
 let playerScoreCount = 0;
 let computerScoreCount = 0;
 let roundCount = 1;
+let isAnimating = false;
 
 
 // Hand Map: each key beats it's value.
@@ -38,16 +43,16 @@ const getComputerChoice = () => {
 
 
 /**
- * Checks if either player or computer has reached 3 wins to end the game.
+ * Checks if either player or computer has reached 2 wins to end the game.
  * Updates the UI to show the winner and hide game elements.
  */
 function checkWinCondition() {
-    if (playerScoreCount === 3) {
+    if (playerScoreCount === 2) {
         choices.style.display = "none";
         quitButton.style.display = "none";
         resultMessage.textContent = "PLAYER WINS THE GAME!!! 🎉";
         playAgainButton.style.display = "block";
-    } else if (computerScoreCount === 3) {
+    } else if (computerScoreCount === 2) {
         choices.style.display = "none";
         quitButton.style.display = "none";
         resultMessage.textContent = "Computer Wins, Game Over...";
@@ -68,13 +73,13 @@ function compareChoice(playerChoice, computerChoice) {
     } else if (handMap[playerChoice] === computerChoice) {
         playerScoreCount++;
         roundCount++;
-        roundIndicator.textContent = Math.min(roundCount, 5);
+        roundIndicator.textContent = Math.min(roundCount, 3);
         playerScore.textContent = playerScoreCount;
         resultMessage.textContent = "You Win!";
     } else {
         computerScoreCount++;
         roundCount++;
-        roundIndicator.textContent = Math.min(roundCount, 5);
+        roundIndicator.textContent = Math.min(roundCount, 3);
         computerScore.textContent = computerScoreCount;
         resultMessage.textContent = "Computer Wins!";
     }
@@ -96,31 +101,58 @@ function initializeGameState() {
     choices.style.display = "grid";
     quitButton.style.display = "block";
     playAgainButton.style.display = "none";
+    playerHand.textContent = "✊";
+    computerHand.textContent = "✊";
 }
 
 
-// Plays a round of the game with the given player choice.
-function playGame(playerChoice) {
-    let computerChoice = getComputerChoice();
+// Helper function to retrieve hand(s) for UI updates.
+const getEmoji = (choice) => {
+    if (choice === "rock") return "✊";
+    if (choice === "paper") return "🖐";
+    if (choice === "scissors") return "✌️";
+}
+
+
+// Plays a round of the game with the given player and computer choice.
+function playGame(playerChoice, computerChoice) {
     compareChoice(playerChoice, computerChoice);
     checkWinCondition();
 }
 
 
-// When clicked, retrieves the player's choice from the button's data attribute and starts the game round.
+/**
+ * Plays the shaking animation for the player and computer hands. Prevents user from repeatedly clicking a choice button while the animation is playing. Uses a delay to provide the UI enough time to update the UI before the player can play another round.
+ * @param {HTMLButtonElement} button - The clicked choice button. 
+ */
+function playAnimation(button) {
+    if (isAnimating) return;
+    let playerChoice = button.dataset.choice;
+    let computerChoice = getComputerChoice();
+
+    playerHand.textContent = "✊";
+    computerHand.textContent = "✊";
+
+    playerHand.classList.add("shake");
+    computerHand.classList.add("shake");
+    isAnimating = true;
+
+    setTimeout(() => {
+        playerHand.classList.remove("shake");
+        computerHand.classList.remove("shake");
+        playerHand.textContent = getEmoji(playerChoice);
+        computerHand.textContent = getEmoji(computerChoice);
+        playGame(playerChoice, computerChoice);
+        isAnimating = false;
+    }, 1000);
+}
+
+
+// Event Listeners
 choiceButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        let playerChoice = button.dataset.choice;
-        playGame(playerChoice);
-    });
+    button.addEventListener("click", () => playAnimation(button));
 });
-
-
-// Resets game state when clicked.
 quitButton.addEventListener("click", () => initializeGameState());
-
-
-// Resets the game state to start a new game when clicked.
 playAgainButton.addEventListener("click", () => initializeGameState());
 
 
